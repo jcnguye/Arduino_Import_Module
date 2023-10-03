cf90c9ecaac90657bb9141a8deffff79627a90ca
Rachel <rbreinho@asu.edu>
2023-10-03 08:53:50 -0700

    US#4 completed

--- arduino-mod/src/extension.ts
+++ arduino-mod/src/extension.ts
@@ -41,10 +41,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Arduino_Mod!');
-		let dirPath: string = 'C:\Users\rsbre\Documents\Swim Team';
-		for (const filePath of getAllFilePaths(dirPath)) {
-			console.log(filePath);
-		}
+
 	});
 
 	context.subscriptions.push(disposable);
