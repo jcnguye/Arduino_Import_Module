df1e2fb95ac0da6a33b918bfab9e69b818ae9d92
Cole <cmalachu@asu.edu>
2023-10-13 17:59:55 -0700

    US#15 added core library file copier

--- arduino-mod/src/extension.ts
+++ arduino-mod/src/extension.ts
@@ -69,10 +69,57 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Arduino_Mod!');
+		
+		/* testing for the library copier
+		if(process.platform == "win32") {
+			librariesCopy("%HOMEPATH%/test")
+		} else {
+			librariesCopy("~/test")
+		}
+		*/
+		
 	});
 
 	context.subscriptions.push(disposable);
 }
+
+
+
+/**
+ * This copies all of the libraries from Arduino to the user's new local project directory (into the "core" folder
+ * @param dest the user's project directory (/core/libraries will be added)
+ */
+function librariesCopy(dest: string) {
+	// path info: https://support.arduino.cc/hc/en-us/articles/360018448279-Open-the-Arduino15-folder
+	let srcPath = "/Arduino15/packages/DxCore/hardware/megaavr/*/libraries/" // using * because Arduino IDE will only install one version at a time.
+	let runCmd = ""
+	dest = dest + "/core/libraries"
+	if(process.platform == "win32") {
+		srcPath = "%LocalAppData%" + srcPath
+		runCmd = "ROBOCOPY /CREATE /E"
+		srcPath = srcPath.replace("*", "1.5.10") // for some reason windows doesn't like this, so we have to hardcode the path in anyways
+	} else if(process.platform == "darwin") {
+		srcPath = "~/Library" + srcPath
+		runCmd = "mkdir -p " + dest + "&& cp -rc"
+	} else if(process.platform == "linux") {
+		srcPath = "~" + srcPath.replace("Arduino15", ".arduino15")
+		runCmd = "mkdir -p " + dest + " && cp -r"
+	}
+	const command = runCmd + " " + srcPath + " " + dest
+	const { exec } = require('child_process');
+	exec(command, (error: string, stdout: string, stderr: string) => {
+		if(error) {
+			// windows will say the command failed, but it didn't
+			vscode.window.showInformationMessage('An error occured trying to copy the library files: ' + error);
+			return
+		}
+		console.log("copy command output: " + stdout)
+		if(stderr) {
+			console.log("copy command output: " + stderr)
+		}
+	})
+}
+
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
