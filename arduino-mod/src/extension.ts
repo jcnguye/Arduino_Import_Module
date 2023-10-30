32472b780592cde8a23d0ad32441c1dd1d7240ba
J. Scotty <jscottysolomon@gmail.com>
2023-10-29 21:53:27 -0700

    Beginning key-pair matching between variables in platform.txt and boards.txt

--- arduino-mod/src/extension.ts
+++ arduino-mod/src/extension.ts
@@ -47,7 +47,7 @@ async function parsePlatform(filePath:string) {
         ////// get all the compiler flag lines
         for (var i = 0; i < lines.length; i++) {
             var line = lines[i].substring(0, lines[i].indexOf('='));
-            if (line.includes("compiler.") && line.includes(".flags")) {
+            if (line.includes("compiler.") || line.includes(".flags") || line.includes("recipe.") || line.includes("build")) {
                 flagArr.push(lines[i]);
             }
         }
@@ -147,28 +147,17 @@ function parseBoards(filepath: string, boardName: string): Promise<Map<string, s
         const regex = /[^#]*=/
        
         rl.on('line', (line) => {
-            // console.log(line)
+        
             const matches = line.match(regex);
 
             if(matches) {
                 var str = line;
-                var mapValues = str.split('=');
+                var mapValues = str.split('=',2);
                 
-                // const subKeys = /{*}/
-                // const flags = /-/
-                // const option = /^[^\s]/
                 if(mapValues[0].includes(boardName)) {
                     map.set(mapValues[0],mapValues[1]);
                 }
                 
-
-                // if(!mapValues[1].includes(' ')) {
-                //     map.set(mapValues[0],mapValues[1])
-                // } else if(mapValues[1].includes('{') || mapValues[1].includes('-')) {
-                //     map.set(mapValues[0],mapValues[1])
-                // } else {
-                //     console.log(mapValues[1], "\t:", mapValues[1]);
-                // }
             }
             
         });
@@ -182,54 +171,115 @@ function parseBoards(filepath: string, boardName: string): Promise<Map<string, s
 
 async function hashing(version: string) {
     try {
+        //calling boards.txt and platform.txt parser functions
         const localAppData = process.env.LOCALAPPDATA;
         const libraryFilePath = path.join(localAppData, "Arduino15", "packages", "DxCore","hardware","megaavr",version,"boards.txt");
         const map = await parseBoards(libraryFilePath, "avrdd");
-        // console.log("testing");
 
-        // const regex = /{([^{}]*)}/g;
-        const regex = /{([^}]+)}/g;
-        // const regex = /{([^{}}]*)}/g;
-        // const regex = /\{[+]\}/g;
-        let temp;
+        let platform_folder = path.join(localAppData, "Arduino15", "packages", "DxCore","hardware","megaavr",version);
+        let array = await parsePlatform(platform_folder);
+
+        let cppPatternIndex = 0;
+        let recipe = "recipe.cpp.o.pattern";
+        
+
+        //finding cpp recipe line
+        for(let i =0; i < array.length; i++) {
+            if(array[i].includes(recipe)) {
+                cppPatternIndex = i;
+                // console.log(array[i]);
+                break;
+            }
+        } 
+        
+        //getting all flags and variables from line
+        let optionString = array[cppPatternIndex].substring(recipe.length+1,array[cppPatternIndex].length);
+        let optionArray = optionString.split(" ");
+
+        let variables: string[] = [];   //{variable}, without {}
+        let standAloneFlags = [];       //-flag
+        let flagAndVariables = [];      //mmcu={variable}, with {}
+
+        //iterating through paltform.txt for variables
+        for(let i = 0; i < optionArray.length; i++) {
+            let str = optionArray[i];
+            //checking for complex variables
+            if(str.includes("=") && str.includes("{") {
+                flagAndVariables.push(optionArray[i]);
+            }
+            //filtering for relevant simple variables
+            if(str.includes("{") && !str.includes("source_file") && !str.includes("includes") && 
+                !str.includes("source_files") && !str.includes("object_file") && !str.includes("core.path")) {
+                variables.push(optionArray[i].substring(1,optionArray[i].length-1))
+            }
+        }
+
+        //DEBUG
+        console.log(variables);
+        console.log(flagAndVariables);
+        // console.log(options);
+
+        //reiterating through platform.txt for variables contained both in recipe and platform.txt
+        for(let i = 0; i < array.length; i++) {
+            let equalIndex = array[i].indexOf("=");
+
+            let value = array[i].substring(0,equalIndex);
+            let options = array[i].substring(equalIndex+1,array[i].length);
+
+            let index = variables.indexOf(value);
+            if(index != -1) {
+                
+                let parsedOptions = options.split(" ");
+                
+                for(let i = 0; i < parsedOptions.length; i++) {
+                    let opt = parsedOptions[i];
+                    //flags
+                    if(!opt.includes("{") && opt.includes("-")) {
+                        standAloneFlags.push(opt);
+                    } else if(opt.indexOf("{") != 0 && opt != "") {
+                        //copmlex variables
+                        const match = opt.match(/{(.*?)}/);
+                        flagAndVariables.push(opt);
+                        variables.push(match[1]);
+                    } else if(opt != "") {
+                        //simple variables
+                        variables.push(opt.substring(1,opt.length-1));
+                    }
+                }
+            }
+        }
+
+        //DEBUG
+        console.log("\n\n");
+        console.log(variables);
+        console.log(standAloneFlags);
+        console.log(flagAndVariables)
     
+        //iterating through boards.txt for variable values and new variables within platform.txt variables
         map.forEach((value, key) => {
-            let matches = value.match(regex);
-            if(matches) {
-                console.log(matches);
-            } else {
-                console.log(`Key: ${key}, Value: ${value}`);
+            let splitIndex = -1;
+            for(let i = 0; i < variables.length; i++) {
+                if(key.includes("build.flmapopts")) {
+
+                } else if(key.includes("build.attachmode")) {
+
+                } else if(key.includes("wiremode")) {
+
+                } else if(key.includes("millistimer")) {
+
+                } else if(key.includes("clocksource")) {
+
+                } else if(key.includes(variables[i]) && !key.includes("oldversion")) {
+                    console.log(key + ":   " + value);
+                }
+                
             }
 
-
-
-            let options = []
-            // match = []
-            // if(value.includes('{')) {
-            //     console.log(`Key: ${key}, Value: ${value}`);
-
-            // }
-            // if(value.match(regex)) {
-            //     // console.log(`Key: ${key}, Value: ${value}`);
-            //     var empty = value.match(regex);
-            //     console.log("tester: ", empty);
-            // } else {
-            //     console.log(`Key: ${key}, Value: ${value}`); 
-            // }
-
-            // while( (temp = regex.exec(value)) !== null) {
-            //     // console.log(temp);
-            //     options.push(temp[0]);
-            //     console.log("index: ", temp);
-            // }
-            
-            // var test = value.match(regex);
-            // console.log("test: ", test);
-
-            // console.log(options);
-
-            // console.log(regex.exec(value));
+            //TODO: add variable values to flags[] and remove simple variables from array
+            //TODO: replace variables names in complex variables with their values and add to flag array
         });
+
+        //TODO: reiterate through map for newly found variable values
     } catch (error) {
         console.error("An error occurred:", error);
     }
