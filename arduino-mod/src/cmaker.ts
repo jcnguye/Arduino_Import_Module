69e0a7050a45853e198279eecde71b98661ae7f3
Cole <cmalachu@asu.edu>
2023-10-31 11:55:45 -0700

    US#47#51 Added simple src file setter to cmaker

--- arduino-mod/src/cmaker.ts
+++ arduino-mod/src/cmaker.ts
@@ -8,11 +8,15 @@ import * as fs from 'fs';
  * 
  */
 export function cmakeSkeleton(projDir: string, projName: string) {
-	let data = "cmake_minimum_required(VERSION 3.0)"
-	data = data + '\nset(CMAKE_C_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-gcc")'
-	data = data + '\nset(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-gcc")'
-	data = data + "\nproject(" + projName + ")"
-	console.log("now write to the file?")
-	fs.writeFileSync(projDir + "/CMakeLists.txt", data);
+	let cmakeHeader = "cmake_minimum_required(VERSION 3.0)"
+	cmakeHeader = cmakeHeader + '\nset(CMAKE_C_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-gcc")'
+	cmakeHeader = cmakeHeader + '\nset(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-gcc")'
+	cmakeHeader = cmakeHeader + "\nproject(" + projName + ")"
+	fs.writeFileSync(projDir + "/CMakeLists.txt", cmakeHeader);
 	// use fs.appendFileSync(projDir + "/CMakeLists.txt", data); for future appends
+}
+
+export function addSourceFile(projDir: string, projName: string, srcName: string) {
+	let cmakeSrc = "\nadd_executable(" + projName + " " + srcName +")"
+	fs.appendFileSync(projDir + "/CMakeLists.txt", cmakeSrc);
 }
