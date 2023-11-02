5d8794ce7e1826d8f1dcf44f3b16f1fb7c1fa92c
Cole <cmalachu@asu.edu>
2023-11-02 12:05:19 -0700

    US#47#50 Add Compilation and Linker flag setters for cmake

--- arduino-mod/src/cmaker.ts
+++ arduino-mod/src/cmaker.ts
@@ -10,7 +10,7 @@ import * as fs from 'fs';
 export function cmakeSkeleton(projDir: string, projName: string) {
 	let cmakeHeader = "cmake_minimum_required(VERSION 3.0)"
 	cmakeHeader = cmakeHeader + '\nset(CMAKE_C_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-gcc")'
-	cmakeHeader = cmakeHeader + '\nset(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-gcc")'
+	cmakeHeader = cmakeHeader + '\nset(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-g++")'
 	cmakeHeader = cmakeHeader + "\nproject(" + projName + ")"
 	fs.writeFileSync(projDir + "/CMakeLists.txt", cmakeHeader);
 	// use fs.appendFileSync(projDir + "/CMakeLists.txt", data); for future appends
@@ -18,5 +18,15 @@ export function cmakeSkeleton(projDir: string, projName: string) {
 
 export function addSourceFile(projDir: string, projName: string, srcName: string) {
 	let cmakeSrc = "\nadd_executable(" + projName + " " + srcName +")"
+	fs.appendFileSync(projDir + "/CMakeLists.txt", cmakeSrc);
+}
+
+export function addCompilerFlags(projDir: string, projName: string, flags: string) {
+	let cmakeSrc = "\ntarget_compile_options(" + projName + " PRIVATE " + flags +")"
+	fs.appendFileSync(projDir + "/CMakeLists.txt", cmakeSrc);
+}
+
+export function addLinkerFlags(projDir: string, projName: string, flags: string) {
+	let cmakeSrc = "\ntarget_link_libraries(" + projName + " " + flags +")"
 	fs.appendFileSync(projDir + "/CMakeLists.txt", cmakeSrc);
 }
