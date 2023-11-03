0ea1403145b159a1a4b1db5a36f7453f88e6310b
srobrie2 <srobrie2@asu.edu>
2023-11-02 22:58:35 -0700

    [US38T40] Task 41 Static Library creation

--- arduino-mod/src/main_cpp.cpp
+++ arduino-mod/src/main_cpp.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 #include <cstdlib>
 
-void compileLibrary(const std::string& sourceFile, const std::string& objectFile) {
+// compiles library files into .o files
+void compileOFile(const std::string& libraryFile, const std::string& objectFileName) {
     // Construct the compilation command
-    std::string compileCommand = "g++ -c " + sourceFile + " -o " + objectFile;
+    std::string compileCommand = "g++ -c " + libraryFile + " -o " + objectFileName+".o";
 
     // Execute the compilation command
     int result = std::system(compileCommand.c_str());
 
     // Check the result of the compilation
     if (result == 0) {
-        std::cout << "Compilation successful: " << objectFile << std::endl;
+        std::cout << "Compilation successful: " << objectFileName << std::endl;
     } else {
-        std::cerr << "Compilation failed for: " << sourceFile << std::endl;
+        std::cerr << "Compilation failed for: " << libraryFile << std::endl;
+    }
+}
+
+// creates static library (.a) from .o files
+void createStaticLibrary(){
+    int libraryResult = system("lib /OUT:mylibrary.lib *.o");
+
+    if (libraryResult != 0) {
+        std::cerr << "Error: Library creation failed." << std::endl;
+    } else {
+        std::cout << "Static library created: mylibrary.lib" << std::endl;
     }
 }
 
@@ -23,7 +35,7 @@ int main(int argc, char* argv[]) {
     }
 
     // Call compileLibrary with command-line arguments
-    compileLibrary(argv[1], argv[2]);
+    compileOFile(argv[1], argv[2]);
 
     return 0;
 }
