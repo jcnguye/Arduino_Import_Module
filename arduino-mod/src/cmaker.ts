de445bf88bdd2353be8e794b64b2c8bc5c012726
Rachel <rbreinho@asu.edu>
2023-11-13 18:50:51 -0700

    US#64T#94 Connect CMake to UI

--- arduino-mod/src/cmaker.ts
+++ arduino-mod/src/cmaker.ts
@@ -16,17 +16,17 @@ export class Cmaker {
 		this.compilerflags = "";
 		this.linkerflags = "";
 	}
-	public setProjectDirectory(ProjectDirectory:string){
-		this.projDir = ProjectDirectory;
+	public setProjectDirectory(projectDirectory:string){
+		this.projDir = projectDirectory;
 	}
-	public setProjectName(ProjectName:string){
-		this.projName = ProjectName;
+	public setProjectName(projectName:string){
+		this.projName = projectName;
 	}
-	public setSourceName(SourceFileName:string){
-		this.srcFileName = SourceFileName;
+	public setSourceName(sourceFileName:string){
+		this.srcFileName = sourceFileName;
 	}
-	public setCompilerFlags(CompileFlag:string){
-		this.compilerflags = CompileFlag;
+	public setCompilerFlags(compileFlag:string){
+		this.compilerflags = compileFlag;
 	}
 	public setLinkerFlags(linkerFlags:string){
 		this.linkerflags = linkerFlags;
@@ -36,44 +36,44 @@ export class Cmaker {
 	public build(): void{
 
 		//sets the cmake version
-		let cmakeHeader = "cmake_minimum_required(VERSION 3.0)"
-		cmakeHeader = cmakeHeader + '\nset(CMAKE_C_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-gcc")'
-		cmakeHeader = cmakeHeader + '\nset(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-g++")'
-		cmakeHeader = cmakeHeader + "\nproject(" + this.projName + ")"
+		let cmakeHeader = "cmake_minimum_required(VERSION 3.0)";
+		cmakeHeader = cmakeHeader + '\nset(CMAKE_C_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-gcc")';
+		cmakeHeader = cmakeHeader + '\nset(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-g++")';
+		cmakeHeader = cmakeHeader + "\nproject(" + this.projName + ")";
 		//cmake  adding executable 
-		let cmakeSrcExecutable = "\nadd_executable(" + this.projName + " " + this.srcFileName +")"
+		let cmakeSrcExecutable = "\nadd_executable(" + this.projName + " " + this.srcFileName +")";
 		// cmake adding compile option
-		let cmakeSrcCompileOpt = "\ntarget_compile_options(" + this.projName + " PRIVATE " + this.compilerflags +")"
+		let cmakeSrcCompileOpt = "\ntarget_compile_options(" + this.projName + " PRIVATE " + this.compilerflags +")";
 		// cmake link libary
-		let cmakeSrcLinkLib = "\ntarget_link_libraries(" + this.projName + " " + this.linkerflags +")"
+		let cmakeSrcLinkLib = "\ntarget_link_libraries(" + this.projName + " " + this.linkerflags +")";
 		// hex file generator
-		let hex = "add_custom_command(TARGET " + projName + " POST_BUILD COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-objcopy -O ihex -R .eeprom " + projName + " " + projName + ".hex)\n"
+		let hex = "add_custom_command(TARGET " + this.projName + " POST_BUILD COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-objcopy -O ihex -R .eeprom " + this.projName + " " + this.projName + ".hex)\n";
 		// bin file generator
-		let bin = "add_custom_command(TARGET " + projName + " POST_BUILD COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-objcopy -O binary -R .eeprom " + projName + " " + projName + ".bin)\n"
+		let bin = "add_custom_command(TARGET " + this.projName + " POST_BUILD COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-objcopy -O binary -R .eeprom " + this.projName + " " + this.projName + ".bin)\n";
 
 		//resets Cmake File
 		if (fs.existsSync(this.projDir + "/CMakeLists.txt")) {
-			fs.unlinkSync(this.projDir + "/CMakeLists.txt")
+			fs.unlinkSync(this.projDir + "/CMakeLists.txt");
 		}
 		if (fs.existsSync(this.projDir + "/Makefile")) {
-			fs.unlinkSync(this.projDir + "/Makefile")
+			fs.unlinkSync(this.projDir + "/Makefile");
 		}
 		if (fs.existsSync(this.projDir + "/cmake_install.cmake")) {
-			fs.unlinkSync(this.projDir + "/cmake_install.cmake")
+			fs.unlinkSync(this.projDir + "/cmake_install.cmake");
 		}
 		if (fs.existsSync(this.projDir + "/CMakeCache.txt")) {
-			fs.unlinkSync(this.projDir + "/CMakeCache.txt")
+			fs.unlinkSync(this.projDir + "/CMakeCache.txt");
 		}
 		if (fs.existsSync(this.projDir + "/CMakeFiles")) {
-			fs.rmSync(this.projDir + "/CMakeFiles", { recursive: true, force: true })
+			fs.rmSync(this.projDir + "/CMakeFiles", { recursive: true, force: true });
 		}
 
 		fs.writeFileSync(this.projDir + "/CMakeLists.txt", cmakeHeader);
 		fs.appendFileSync(this.projDir + "/CMakeLists.txt", cmakeSrcExecutable);
 		fs.appendFileSync(this.projDir + "/CMakeLists.txt", cmakeSrcCompileOpt);
 		fs.appendFileSync(this.projDir + "/CMakeLists.txt", cmakeSrcLinkLib);
-		fs.appendFileSync(projDir + "/CMakeLists.txt", hex);
-		fs.appendFileSync(projDir + "/CMakeLists.txt", bin);
+		fs.appendFileSync(this.projDir + "/CMakeLists.txt", hex);
+		fs.appendFileSync(this.projDir + "/CMakeLists.txt", bin);
 
 		// use fs.appendFileSync(projDir + "/CMakeLists.txt", data); for future appends
 
--- arduino-mod/src/extension.ts
+++ arduino-mod/src/extension.ts
@@ -214,22 +214,10 @@ export function activate(context: vscode.ExtensionContext) {
       MainPanel.render(context.extensionUri);
     });
     context.subscriptions.push(arduinoImportCommand);
-  
-    vscode.commands.registerCommand('arduino-mod.CMakeSetUp', () => {
-      const cmake= new Cmaker();
-      cmake.setProjectDirectory("C:\Users\nguye\Desktop\SER\SER2023_SPRING\SER_CAP\Arduino_Import_Module\arduino-mod");
-      cmake.setProjectName("testproj");
-      cmake.setSourceName("sketch.cpp");
-      cmake.setCompilerFlags('-c -g -Os -Wall -std=gnu++17 -fpermissive -Wno-sized-deallocation -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -Wno-error=narrowing -MMD -flto -mrelax -mmcu=avr64dd32 -DF_CPU=24000000L -DCLOCK_SOURCE=0 -DTWI_MORS_SINGLE -DMILLIS_USE_TIMERB2 -DCORE_ATTACH_ALL -DLOCK_FLMAP -DFLMAPSECTION1 -DARDUINO=10607 -DARDUINO_avrdd -DARDUINO_ARCH_MEGAAVR -DDXCORE="1.5.10" -DDXCORE_MAJOR=1UL -DDXCORE_MINOR=5UL -DDXCORE_PATCH=10UL -DDXCORE_RELEASED=1 -DMVIO_ENABLED -I/Users/Cole/Library/Arduino15/packages/DxCore/hardware/megaavr/1.5.10/cores/dxcore/api/deprecated -I/Users/Cole/Library/Arduino15/packages/DxCore/hardware/megaavr/1.5.10/cores/dxcore -I/Users/Cole/Library/Arduino15/packages/DxCore/hardware/megaavr/1.5.10/variants/32pin-ddseries');
-      cmake.setLinkerFlags('-Wall -Wextra -Os -g -flto -fuse-linker-plugin -mrelax -Wl,--gc-sections,--section-start=.text=0x0,--section-start=.FLMAP_SECTION1=0x8000,--section-start=.FLMAP_SECTION2=0x10000,--section-start=.FLMAP_SECTION3=0x18000 -mmcu=avr64dd32');
-      cmake.build();
-      vscode.window.showInformationMessage("Setting of Cmake file structure.");
-
-    });
 }
 
 
-export function startImport(sketchPath: string, destDir: string, board: Board) {
+export async function startImport(sketchPath: string, destDir: string, board: Board) {
     vscode.window.showInformationMessage("Starting import.");
     //rename .ino as .cpp and copy it to the destination directory
     const file = path.basename(sketchPath);
@@ -259,11 +247,17 @@ export function startImport(sketchPath: string, destDir: string, board: Board) {
     importproj.copyAvrGcc(destDir);
     console.log("Compiler copy complete");
 
+    const cmake= new Cmaker();
+    cmake.setProjectDirectory(destDir);
+    cmake.setProjectName(file);
+    cmake.setSourceName(file);
+    cmake.setCompilerFlags(await parser.getAllFlags(board));
+
+    //TOD - this needs to be fixed to link correct files
+    cmake.setLinkerFlags('-Wall -Wextra -Os -g -flto -fuse-linker-plugin -mrelax -Wl,--gc-sections,--section-start=.text=0x0,--section-start=.FLMAP_SECTION1=0x8000,--section-start=.FLMAP_SECTION2=0x10000,--section-start=.FLMAP_SECTION3=0x18000 -mmcu=avr64dd32');
+    cmake.build();
+
     vscode.window.showInformationMessage("Import complete!");
-
-    //TODO - change usage so flags are added to CMAKE
-    printFlags(board);
-
 }
 
 
