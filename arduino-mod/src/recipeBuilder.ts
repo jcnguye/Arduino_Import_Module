e8b39716fd171498c675147c5e7d0c740c43c766
jcnguye <nguyenj48@gmail.com>
2024-02-08 00:45:31 -0700

    made a recipe class that handles the format and build of the recipe

--- arduino-mod/src/recipeBuilder.ts
+++ arduino-mod/src/recipeBuilder.ts
@@ -4,37 +4,63 @@ import { Board } from './board';
 //Purpose of this class is to read the platform.txt and board.txt file 
 //and return the format of the recipes in the platform.txt 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export class recipe {
+export class Recipe {
     private board: Board;
-    //key being the variable thats being replaced and value being the update variable thats being placed
-    private const replacements = {
-        'build.mcu': 'atmega328p',
-        'build.f_cpu': '16000000L',
-        'runtime.ide.version': '10607',
-        'build.board': 'AVR_NANO',
-        'build.arch': 'AVR',
-        
-    };
 
-    
+    //key being the variable thats being replaced and value being the update variable thats being placed
+    private replacements: { [key: string]: string }; // Define replacements property
     constructor(board: Board){
         this.board = board;
+        this.replacements = {
+            '{build.mcu}': 'atmega328p',
+            '{runtime.ide.version}': '10607',
+            '{build.board}': 'AVR_NANO',
+            '{build.arch}': 'AVR',
+            '{compiler.c.flags}': 'compiler.c.flags=-c -g -Os {compiler.warning_flags} -std=gnu11 -ffunction-sections -fdata-sections -MMD -flto -fno-fat-lto-objects',
+        };
+       
 	}
 
-    public replaceVariables(template: string, replacements: { [key: string]: string }): string {
+    public replaceVariables(recipeString: string): string {
     // Construct a regular expression to match strings within {}
     const regex = /{([^}]+)}/g;
-
     // Replace each matched string with the corresponding value from the replacements object
-    return template.replace(regex, (match, key) => {
+    return recipeString.replace(regex, (match, key) => {
         // If the key exists in replacements, return its value; otherwise, return the original match
-        return replacements.hasOwnProperty(key) ? replacements[key] : match;
+        return this.replacements.hasOwnProperty(key) ? this.replacements[key] : match;
     });
+
+    }
+
+
+
+    /*
+    funtion that will format the C compiler recipe with all needed flags
+    */
+    formatCCompilerBuild(cRecipeString:String): String {
+
+        console.log(this.board.getTargetBoardFlagHelper( "nano.build.variant",this.board.getBoardflagsNano(this.board.getPathToBoardFile())));
+
+
+        let cRecipeStringArr = cRecipeString.split(' ');
+        let newFormatStringArr = [];
+        
+        console.log("\nPrinting out recipe \n");
+
+        for (const str of cRecipeStringArr.slice(2,7)) {
+            let splitStr = str.split("=");
+            if(this.replacements.hasOwnProperty(splitStr[0])){
+                
+            }
+            const replacedString = str.replace("{build.mcu}", "avr");
+            newFormatStringArr.push(str);
+            console.log(str);
+        }
+        return "";
     }
 
 
 }
-
 
 
 // Example usage
@@ -54,5 +80,5 @@ const replacements1 = {
     'object_file': 'main.o'
 };
 
-const replacedRecipe = replaceVariables(recipe1, replacements1);
-console.log(replacedRecipe);
+//const replacedRecipe = replaceVariables(recipe1, replacements1);
+//console.log(replacedRecipe);
