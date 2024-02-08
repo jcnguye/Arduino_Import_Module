220607bf169c7d491fd7ef656cfd5de1812caefa
jcnguye <nguyenj48@gmail.com>
2024-02-08 00:46:08 -0700

    added recipe to cmaker

--- arduino-mod/src/cmaker.ts
+++ arduino-mod/src/cmaker.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 import { Board } from './board';
+import { Recipe } from './recipeBuilder';
+import * as parser from './parser';
 
 export class Cmaker {
 	public projDir: string;
@@ -10,68 +12,77 @@ export class Cmaker {
 	public compilerflags: string;
 	public linkerflags: string;
 	private board: Board;
-	
-	constructor(board: Board){
+	private recipe: Recipe;
+
+	constructor(board: Board) {
 		this.projDir = "";
 		this.projName = "";
 		this.srcFileName = "";
 		this.compilerflags = "";
 		this.linkerflags = "";
-		this.board = board; 
+		this.board = board;
+		this.recipe = new Recipe(board);
 	}
-	public setProjectDirectory(projectDirectory:string){
+	public setProjectDirectory(projectDirectory: string) {
 		this.projDir = projectDirectory;
 	}
-	public setProjectName(projectName:string){
+	public setProjectName(projectName: string) {
 		this.projName = projectName;
 	}
-	public setSourceName(sourceFileName:string){
+	public setSourceName(sourceFileName: string) {
 		this.srcFileName = sourceFileName;
 	}
-	public setCompilerFlags(compileFlag:string){
+	public setCompilerFlags(compileFlag: string) {
 		this.compilerflags = compileFlag;
 	}
-	public setLinkerFlags(linkerFlags:string){
+	public setLinkerFlags(linkerFlags: string) {
 		this.linkerflags = linkerFlags;
 	}
 
 
-	public build(): void{
+	public build(): void {
 
 		//sets the cmake version
 		let cmakeHeader = "cmake_minimum_required(VERSION 3.8)\n";
-		
+
 		const binPath = path.join(this.board.getPathToCompiler(), "bin");
 		cmakeHeader = cmakeHeader + 'set(CMAKE_C_COMPILER ' + path.join(binPath, "avr-gcc.exe").replace(/\\/g, '/') + ')\n';
-		cmakeHeader = cmakeHeader + 'set(CMAKE_CXX_COMPILER ' + path.join(binPath, "avr-g++.exe").replace(/\\/g, '/') +')\n';
+		cmakeHeader = cmakeHeader + 'set(CMAKE_CXX_COMPILER ' + path.join(binPath, "avr-g++.exe").replace(/\\/g, '/') + ')\n';
 
 		cmakeHeader = cmakeHeader + 'project(' + this.projName + ' C CXX)\n\n';
 		cmakeHeader = cmakeHeader + 'set(CORE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/core)\n\n';
-		
-		cmakeHeader = cmakeHeader + 'set(CMAKE_AR ' + path.join(binPath, "avr-gcc-ar.exe").replace(/\\/g, '/') +')\n';
-		cmakeHeader = cmakeHeader + 'set(CMAKE_OBJCOPY ' + path.join(binPath, "avr-objcopy.exe").replace(/\\/g, '/') +')\n\n';
 
-		cmakeHeader = cmakeHeader + 'set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -c -g -Os -w -std=gnu++11 -fpermissive -fno-exceptions ' + 
-		'-ffunction-sections -fdata-sections -fno-threadsafe-statics -Wno-error=narrowing -MMD -flto -mmcu=atmega328p -DF_CPU=16000000L '+ 
-		'-DARDUINO=10607 -DARDUINO_AVR_NANO -DARDUINO_ARCH_AVR")\n';
-
-		cmakeHeader = cmakeHeader + 'set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -c -g -Os -w -std=gnu11 -ffunction-sections -fdata-sections -MMD ' + 
-		'-flto -fno-fat-lto-objects -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10607 -DARDUINO_AVR_NANO -DARDUINO_ARCH_AVR")\n';
+		let recipeString = this.board.getPlatformCCompilerRecipePattern(this.board.getPathToPlatformFile());
+		console.log("testing recipe\n");
+		console.log(this.recipe.formatCCompilerBuild(recipeString));
 		
+		console.log("--------- testing parser board ----------\n");
+
+
+		cmakeHeader = cmakeHeader + 'set(CMAKE_AR ' + path.join(binPath, "avr-gcc-ar.exe").replace(/\\/g, '/') + ')\n';
+		cmakeHeader = cmakeHeader + 'set(CMAKE_OBJCOPY ' + path.join(binPath, "avr-objcopy.exe").replace(/\\/g, '/') + ')\n\n';
+
+		cmakeHeader = cmakeHeader + 'set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -c -g -Os -w -std=gnu++11 -fpermissive -fno-exceptions ' +
+			'-ffunction-sections -fdata-sections -fno-threadsafe-statics -Wno-error=narrowing -MMD -flto -mmcu=atmega328p -DF_CPU=16000000L ' +
+			'-DARDUINO=10607 -DARDUINO_AVR_NANO -DARDUINO_ARCH_AVR")\n';
+
+		cmakeHeader = cmakeHeader + 'set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -c -g -Os -w -std=gnu11 -ffunction-sections -fdata-sections -MMD ' +
+			'-flto -fno-fat-lto-objects -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10607 -DARDUINO_AVR_NANO -DARDUINO_ARCH_AVR")\n';
+
 		cmakeHeader = cmakeHeader + 'set(CMAKE_STATIC_LIBRARY_FLAGS "rcs")\n';
 		cmakeHeader = cmakeHeader + 'set(CMAKE_C_FLAGS_LINKER "${CMAKE_C_FLAGS_LINKER} -w -Os -g -flto -fuse-linker-plugin -Wl,--gc-sections ' +
-		'-mmcu=atmega328p -o ${CMAKE_CURRENT_SOURCE_DIR}/build/CMakeFiles/' + this.projName + '.dir/' + this.projName + 
-		'.elf ${CMAKE_CURRENT_SOURCE_DIR}/build/CMakeFiles/' + this.projName + '.dir/' + this.projName + '.cpp.o ${CMAKE_CURRENT_SOURCE_DIR}/build/libcore.a -L${CMAKE_CURRENT_SOURCE_DIR}/build -lm")\n\n';
+			'-mmcu=atmega328p -o ${CMAKE_CURRENT_SOURCE_DIR}/build/CMakeFiles/' + this.projName + '.dir/' + this.projName +
+			'.elf ${CMAKE_CURRENT_SOURCE_DIR}/build/CMakeFiles/' + this.projName + '.dir/' + this.projName + '.cpp.o ${CMAKE_CURRENT_SOURCE_DIR}/build/libcore.a -L${CMAKE_CURRENT_SOURCE_DIR}/build -lm")\n\n';
 
 		//cmake  adding executable 
-		let cmakeSrcExecutable = "add_executable(" + this.projName + " " + this.srcFileName +")\n";
+		let cmakeSrcExecutable = "add_executable(" + this.projName + " " + this.srcFileName + ")\n";
 		// cmake adding compile option
-		let cmakeSrcCompileOpt = "target_compile_options(" + this.projName + " PRIVATE " + this.compilerflags +")\n";
+		let cmakeSrcCompileOpt = "target_compile_options(" + this.projName + " PRIVATE " + this.compilerflags + ")\n";
 		// cmake link libary
-		let cmakeSrcLinkLib = "target_link_libraries(" + this.projName + " " + this.linkerflags +")\n";
+		let cmakeSrcLinkLib = "target_link_libraries(" + this.projName + " " + this.linkerflags + ")\n";
 		// cmake include directories, file, and add library
 		let cmakeDir = 'include_directories("${CMAKE_CURRENT_SOURCE_DIR}/core" "${CMAKE_CURRENT_SOURCE_DIR}/core/eightanaloginputs" "${CMAKE_CURRENT_SOURCE_DIR}/core/standard")\n' +
-		'file(GLOB CORE_SOURCES "${CORE_DIR}/*.cpp" "${CORE_DIR}/*.c")\nadd_library(core STATIC ${CORE_SOURCES})\n\n';
+			'file(GLOB CORE_SOURCES "${CORE_DIR}/*.cpp" "${CORE_DIR}/*.c")\nadd_library(core STATIC ${CORE_SOURCES})\n\n';
 		// hex file generator
 		let hex = "add_custom_command(TARGET " + this.projName + " POST_BUILD COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/core/compiler/bin/avr-objcopy -O ihex -R .eeprom " + this.projName + " " + this.projName + ".hex)\n";
 		hex = hex + 'set(HEX_FILE_OUTPUT_PATH "${CMAKE_CURRENT_BINARY_DIR}/' + this.projName + '.hex")\n';
