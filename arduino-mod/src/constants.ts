9a394e9833e008dd87e27925f866cc781d2130bc
J. Scotty <jscottysolomon@gmail.com>
2024-03-27 12:47:23 -0700

    Constatnts

--- arduino-mod/src/constants.ts
+++ arduino-mod/src/constants.ts
@@ -1,6 +1,7 @@
 export const CMAKE = {
     FILE_NAME : "/CmakeLists.txt",
     VERSION: 'cmake_minimum_required(VERSION 3.28)\n',
+
     SET_C_COMPILER: 'set(CMAKE_C_COMPILER ',
     SET_CXX_COMPILER: 'set(CMAKE_CXX_COMPILER ',
     SET_AR: 'set(CMAKE_AR ',
@@ -23,6 +24,7 @@ export const CMAKE = {
     SET_LIB_DIR: 'set(LIB_DIR ${CMAKE_CURRENT_SOURCE_DIR}/lib)\n\n',
     NANO_INCLUDE: 'include_directories("${CMAKE_CURRENT_SOURCE_DIR}/core" "${CMAKE_CURRENT_SOURCE_DIR}/lib" "${CMAKE_CURRENT_SOURCE_DIR}/core/eightanaloginputs" "${CMAKE_CURRENT_SOURCE_DIR}/core/standard")\n',
     DXCORE_INCLUDE: 'include_directories("${CMAKE_CURRENT_SOURCE_DIR}/core" "${CMAKE_CURRENT_SOURCE_DIR}/core/deprecated" "${CMAKE_CURRENT_SOURCE_DIR}/core/32pin-ddseries")\n',
+    
     ELF_DIR: 'set(ELF_FILE_OUTPUT_PATH "${CMAKE_CURRENT_BINARY_DIR}/output/',
     MAP_DIR: 'set(MAP_FILE_OUTPUT_PATH "${CMAKE_CURRENT_BINARY_DIR}/output/',
     LST_DIR: 'set(LST_FILE_OUTPUT_PATH "${CMAKE_CURRENT_BINARY_DIR}/output/'
