fddde3aa50836b3de5c5f45e57467703bb223623
J. Scotty <jscottysolomon@gmail.com>
2024-04-10 18:52:13 -0700

    Modifying dxcore include to work in depreciated api

--- arduino-mod/src/constants.ts
+++ arduino-mod/src/constants.ts
@@ -29,7 +29,7 @@ export const CMAKE = {
     SET_CORE_DIR: 'set(CORE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/core)\n',
     SET_LIB_DIR: 'set(LIB_DIR ${CMAKE_CURRENT_SOURCE_DIR}/lib)\n\n',
     NANO_INCLUDE: 'include_directories("${CMAKE_CURRENT_SOURCE_DIR}/core" "${CMAKE_CURRENT_SOURCE_DIR}/lib" "${CMAKE_CURRENT_SOURCE_DIR}/core/eightanaloginputs" "${CMAKE_CURRENT_SOURCE_DIR}/core/standard")\n',
-    DXCORE_INCLUDE: 'include_directories("${CMAKE_CURRENT_SOURCE_DIR}/core" "${CMAKE_CURRENT_SOURCE_DIR}/core/deprecated" "${CMAKE_CURRENT_SOURCE_DIR}/core/32pin-ddseries")\n',
+    DXCORE_INCLUDE: 'include_directories("${CMAKE_CURRENT_SOURCE_DIR}/core" "${CMAKE_CURRENT_SOURCE_DIR}/core/api/deprecated" "${CMAKE_CURRENT_SOURCE_DIR}/lib")\n',
     DXCORE_OPTIMIZATION: 'set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)\n',
 
     MAP_GEN: 'set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,-Map=${CMAKE_BINARY_DIR}/output/',
